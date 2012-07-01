06217cda4eb517f022376fa84a81b6b6f3d54059
Ross Bell <rossallenbell@gmail.com>
2012-06-30 23:44:00 -0500

    Import fix

--- src/com/rossallenbell/darkfallgearoptimizer/DarkfallGearOptimizer.java
+++ src/com/rossallenbell/darkfallgearoptimizer/DarkfallGearOptimizer.java
@@ -1,11 +1,9 @@
 
 package com.rossallenbell.darkfallgearoptimizer;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
